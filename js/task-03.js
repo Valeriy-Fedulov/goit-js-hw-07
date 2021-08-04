47f664ec41351dbc3141ea1c330c52fb3ce24d47
vofm <valerafm.vofm@gmail.com>
2021-08-04 22:19:20 +0300

    Update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -8,21 +8,6 @@
 
 // Все элементы галереи должны добавляться в DOM за одну операцию вставки.
 // Добавь минимальное оформление галереи флексбоксами или гридами через css-классы.
-// const images = [
-//   {
-//     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-//     alt: 'White and Black Long Fur Cat',
-//   },
-//   {
-//     url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-//     alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
-//   },
-//   {
-//     url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-//     alt: 'Group of Horses Running',
-//   },
-// ];
-
 
 const images = [
   {
@@ -38,3 +23,10 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const imgEl = document.querySelector('#gallery');
+console.log(imgEl);
+
+const listImg = document.createElement('li');
+listImg.insertAdjacentHTML('afterend', '<div></div>');
+console.log(listImg);
