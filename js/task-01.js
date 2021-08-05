c67b0790cf280ea66d19dedb4d0db7987fc3cb96
vofm <80212894+Valeriy-Fedulov@users.noreply.github.com>
2021-08-05 16:59:43 +0300

    Update task01-06

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,7 @@
-const categorieEl = document.querySelectorAll('.item');
-console.log(`В списке ${categorieEl.length} категории.`);
+const liCategoriesEl = document.querySelectorAll('.item');
+console.log(`В списке ${liCategoriesEl.length} категории.`);
 
-for (const categorie of categorieEl) {
+for (const categorie of liCategoriesEl) {
     console.log(`Категория:`, categorie.querySelector('h2').textContent);
     console.log(`Количество элементов: ${categorie.querySelectorAll('li').length}`);
 }
--- js/task-04.js
+++ js/task-04.js
@@ -1,4 +1,5 @@
-// Счетчик состоит из спана и кнопок, которые должны увеличивать и уменьшать значение счетчика на 1.
+// Счетчик состоит из спана и кнопок, которые должны увеличивать и
+// уменьшать значение счетчика на 1.
 
 // Создай переменную counterValue в которой будет хранится текущее значение счетчика.
 // Создай функции increment и decrement для увеличения и уменьшения значения счетчика
@@ -8,3 +9,38 @@
 //   <span id="value">0</span>
 //   <button type="button" data-action="increment">+1</button>
 // </div>
+ 
+let counterValue = 0;
+
+
+// increment(count) {
+
+// };
+
+// decrement(count) {
+
+// };
+
+const btnDecrement = document.querySelector('button[data-action="decrement"]');
+const btnIncrement = document.querySelector('button[data-action="increment"]');
+const spanEl = document.querySelector('#value');
+
+console.log(spanEl);
+console.log(btnDecrement);
+// console.log(btnDecrement[1].dataset.action);
+const decrement = () => {
+    
+   counterValue -= 1;
+       spanEl.textContent = counterValue;
+    console.log(`count: `, counterValue);
+}
+
+const increment = () => {
+    
+   counterValue += 1;
+       spanEl.textContent = counterValue;
+    console.log(`count: `, counterValue);
+}
+
+btnDecrement.addEventListener('click', decrement);
+btnIncrement.addEventListener('click', increment); 
