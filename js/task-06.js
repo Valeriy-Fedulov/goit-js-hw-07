7d57921ab279c5b99d3f3d744fdecb057dc60ea3
vofm <valerafm.vofm@gmail.com>
2021-08-08 17:11:39 +0300

    Update task-06

--- js/task-06.js
+++ js/task-06.js
@@ -1,6 +1,6 @@
 const inputEl = document.querySelector('#validation-input');
 
 inputEl.addEventListener('blur', () => {
-    inputEl.classList.toggle('valid', inputEl.value.length >= inputEl.dataset.length);
-    inputEl.classList.toggle('invalid', inputEl.value.length < inputEl.dataset.length);
+    inputEl.classList.toggle('valid', inputEl.value.length == inputEl.dataset.length);
+    inputEl.classList.toggle('invalid', inputEl.value.length != inputEl.dataset.length);
 });
