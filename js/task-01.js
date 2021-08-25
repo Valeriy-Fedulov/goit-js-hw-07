5d09c9d77aeea009dc285431b240edf65bb51984
vofm <valerafm.vofm@gmail.com>
2021-08-25 05:54:19 +0300

    Add prettier

--- js/task-01.js
+++ js/task-01.js
@@ -4,4 +4,4 @@ console.log(`В списке ${liCategoriesRef.length} категории.`);
 liCategoriesRef.forEach(categorie => {
     console.log(`Категория:`, categorie.firstElementChild.textContent);
     console.log(`Количество элементов: ${categorie.lastElementChild.children.length}`);
- });
+});
